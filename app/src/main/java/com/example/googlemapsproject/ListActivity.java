f703a06eac3c0ddd0f8f3a336ea5bad64c0770b3
Danny393 <danielacosta393@gmail.com>
2020-03-16 13:24:17 -0400

    Connected ListView to MapView with button

--- app/src/main/java/com/example/googlemapsproject/ListActivity.java
+++ app/src/main/java/com/example/googlemapsproject/ListActivity.java
@@ -1,15 +1,26 @@
 package com.example.googlemapsproject;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     ArrayList<String> events;
     ArrayAdapter<String> eventsAdapter;
     ListView lvEvents;
@@ -48,6 +59,43 @@ public class ListActivity extends AppCompatActivity {
         eventsAdapter = new ArrayAdapter<>(this,  android.R.layout.simple_list_item_1, events);
         lvEvents = findViewById(R.id.lvEvents);
         lvEvents.setAdapter(eventsAdapter);
+
+        if (isServicesOK()) {
+            init();
+        }
+    }
+
+    private void init() {
+        Button btnMap = findViewById(R.id.btnMap);
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ListActivity.this, MapActivity.class));
+            }
+        });
+    }
+
+    public boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(ListActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            // everything is fine and user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            // an error occurred but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occurred but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(ListActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
 
